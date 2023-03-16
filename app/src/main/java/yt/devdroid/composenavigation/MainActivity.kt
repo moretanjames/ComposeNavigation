7ec95e160e667ddf168efb241ac4390acacc6e35
moretanjames <moretanjames@gmail.com>
2023-03-15 20:16:06 -0500

    clean

--- app/src/main/java/yt/devdroid/composenavigation/MainActivity.kt
+++ app/src/main/java/yt/devdroid/composenavigation/MainActivity.kt
@@ -6,18 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.core.view.WindowCompat
-import androidx.lifecycle.viewmodel.compose.viewModel
 import yt.devdroid.composenavigation.ui.Navigation
-import yt.devdroid.composenavigation.ui.home.HomeScreen
-import yt.devdroid.composenavigation.ui.note.NoteScreen
-import yt.devdroid.composenavigation.ui.note.NoteViewModel
 import yt.devdroid.composenavigation.ui.theme.ComposeNavigationTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,7 +19,7 @@ class MainActivity : ComponentActivity() {
             ComposeNavigationTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                   Navigation()
+                    Navigation()
                 }
             }
         }
