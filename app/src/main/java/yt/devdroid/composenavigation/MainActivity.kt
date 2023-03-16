1001fd1f5e044e495308714f6876552a3e11f343
moretanjames <moretanjames@gmail.com>
2023-03-15 20:15:47 -0500

    compose nav implemented

--- app/src/main/java/yt/devdroid/composenavigation/MainActivity.kt
+++ app/src/main/java/yt/devdroid/composenavigation/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : ComponentActivity() {
             ComposeNavigationTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Navigation()
+                   Navigation()
                 }
             }
         }
